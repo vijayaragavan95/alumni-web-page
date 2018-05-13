94ea5a8a5bffd178feb572e4ff1b662f483ea601
Vijayaragavan <vijayaragavan95@gmail.com>
2018-05-13 23:59:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# alumni-web-page
---
+++ src/main/webapp/resources/js/home.js
@@ -0,0 +1,11 @@
+//	function formSubmit() {
+//			document.getElementById("logoutForm").submit();
+//		}
+function errormessage() {
+    var x = document.getElementById("error-div");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
